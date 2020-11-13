1c67e353c6a859d7127ec98d959d9d8567b21d5e
Tioman Gally <tioman.gally@novatec-gmbh.de>
2020-11-13 09:18:30 +0100

    Initial Commit

---
+++ src/main/kotlin/de/gally/demoforr2dbc/DemoForR2dbcApplication.kt
@@ -0,0 +1,11 @@
+package de.gally.demoforr2dbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoForR2dbcApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoForR2dbcApplication>(*args)
+}
